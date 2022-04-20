4256b942bb42ceb25579fca98f21ec3af4f88b69
ⒶⓁⒾⒸⒾⒶ <83991079+chojangg@users.noreply.github.com>
2022-04-21 00:04:31 +0900

    Add files via upload

--- PackageProject/src/package01/PackageExam.java
+++ PackageProject/src/package01/PackageExam.java
@@ -5,8 +5,8 @@ package package01;
 //java.lang 패키지는 import를 하지 않아도 된다.
 
 public class PackageExam {
-	
 	public static void main(String[] args) {
-		
+		String strData = "I Love You";
+		System.out.println("strData = " + strData);
 	}
 }

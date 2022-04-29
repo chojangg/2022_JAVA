628416ff79cb5d31ac06a85d50b5b9eabd06b89d
ⒶⓁⒾⒸⒾⒶ <83991079+chojangg@users.noreply.github.com>
2022-04-29 13:55:47 +0900

    TextInheritance11

---
+++ inheritanceProject/src/TextInheritance11/TV.java
@@ -0,0 +1,21 @@
+package TextInheritance11;
+
+public class TV implements Sound {
+	private int SndLevel;
+	public TV() {
+		SndLevel = 0;
+	}
+	
+	@Override
+	public void SoundUp(int level) {
+		SndLevel += level;
+		System.out.println(level + " TV º¼·ýÁõ°¡ ");
+	}
+
+	@Override
+	public void SoundDown(int level) {
+		SndLevel -= level;
+		if(SndLevel < 0) SndLevel = 0;
+	}
+
+}

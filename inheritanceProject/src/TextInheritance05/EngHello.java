2ea0fe5a0394a0b792a1c2ffc1501cd81597ebca
ⒶⓁⒾⒸⒾⒶ <83991079+chojangg@users.noreply.github.com>
2022-04-29 13:44:53 +0900

    EngHello 수정

--- inheritanceProject/src/TextInheritance05/EngHello.java
+++ inheritanceProject/src/TextInheritance05/EngHello.java
@@ -1,19 +1,21 @@
 package TextInheritance05;
 
-public class EngHello extends Greeting{
-	public String name ="Mr.Hong";
-	public void sayHEllo() {
-		System.out.println(name + "Nice to meet you");
-	}
-	public void test() {
-		System.out.println(super.name);
-		super.sayHello();
+public class EngHello extends Greeting {
+	public String name = "Mr.Hong";
+	
+	public void salHello() {
+		System.out.println(name + " Nice to meet you");
 	}
 	
+	public void test() {
+		System.out.print(super.name); // super.name는 부모클래스의 name을 출력
+		super.salHello(); // 부모클래스에 있는 salHello()메소드를 호출
+	}
 	public static void main(String[] args) {
 		EngHello eng = new EngHello();
-		eng.sayHEllo();
+		eng.salHello();
 		eng.test();
+
 	}
 
 }

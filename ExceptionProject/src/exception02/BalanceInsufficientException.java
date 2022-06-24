ec8a4d6d0b0ad26e3a8eccbf4ab61a5d19c4727b
ⒶⓁⒾⒸⒾⒶ <83991079+chojangg@users.noreply.github.com>
2022-06-24 23:56:39 +0900

    BalanceInsufficientException

---
+++ ExceptionProject/src/exception02/BalanceInsufficientException.java
@@ -0,0 +1,9 @@
+package exception02;
+
+// »ç¿ëÀÚ
+public class BalanceInsufficientException extends Exception {
+	public BalanceInsufficientException() {}
+	public BalanceInsufficientException(String message) {
+		super(message);
+	}
+}
